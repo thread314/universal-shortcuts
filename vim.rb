d128ac2fb5fd2ccb5efab37460126df8f4b1f2f5
Eddie Drury <eddie.drury@gmail.com>
2020-04-24 10:03:35 +0800

    write_shortcut writing full command

--- vim.rb
+++ vim.rb
@@ -3,5 +3,18 @@ vim.unmapcommand = ":unmap <?>"
 vim.mapcommand = ":map <?> <!>"
 vim.dotfilelocation = "~/.vimrc"
 vim.defaultmodifier = "<leader><?>"
+vim.OpenNewTab[:function] = ":tabnew<CR>"
+
+
+
 vim.write_shortcuts
 
+
+
+
+#  :OpenNewTab => {:defaultkey => "t"},
+#  :SelectPreviousTab => {:defaultkey => "j"},
+#  :SelectNextTab => {:defaultkey => "k"},
+#  :ShiftTabLeft => {:defaultkey => ","},
+#  :ShiftTabRight => {:defaultkey => "."},
+#  :CloseCurrentTab => {:defaultkey => "w"}
