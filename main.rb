36e02c66029766e6b2c848f29821d822b45fffec
Eddie Drury <eddie.drury@gmail.com>
2020-04-24 13:39:12 +0800

    all tab keys added and filters out mapcommand etc

--- main.rb
+++ main.rb
@@ -1,24 +1,26 @@
 #Default shortcuts
 class Commands 
-  attr_accessor :opennewtab, :selectprevioustab 
+  attr_accessor :opennewtab, :selectprevioustab, :selectnexttab, :shifttableft, :shifttabright, :closecurrenttab
   def initialize
     @opennewtab = {:defaultkey => "t"}
     @selectprevioustab = {:defaultkey => "j"}
-#  :SelectNextTab => {:defaultkey => "k"},
-#  :ShiftTabLeft => {:defaultkey => ","},
-#  :ShiftTabRight => {:defaultkey => "."},
-#  :CloseCurrentTab => {:defaultkey => "w"}
+    @selectnexttab = {:defaultkey => "k"}
+    @shifttableft = {:defaultkey => ","}
+    @shifttabright = {:defaultkey => "."}
+    @closecurrenttab = {:defaultkey => "w"}
   end
 end
 
+#Assign shortcuts to an individual app
 class App < Commands
   attr_accessor :unmapcommand, :mapcommand, :dotfilelocation, :defaultmodifier
   def write_shortcuts
     self.instance_variables.map do |attribute|
+      if (attribute.to_s == "@unmapcommand") || (attribute.to_s == "@mapcommand") || (attribute.to_s == "@dotfilelocation") || (attribute.to_s == "@defaultmodifier")
+        puts "blank"
+      else
       puts self.instance_variable_get(attribute)[:defaultkey]
       puts self.instance_variable_get(attribute)[:function]
-
-
 #      p attribute
 #      p self[attribute]
 #      if function = key[:function]
@@ -27,6 +29,7 @@ class App < Commands
 #        line = self.mapcommand.gsub("<?>",shortcut)
 #        puts line = line.gsub("<!>", function)
 #      end
+      end
     end
   end
 end
--- vim.rb
+++ vim.rb
@@ -1,9 +1,15 @@
 vim = App.new
-#vim.unmapcommand = ":unmap <?>"
-#vim.mapcommand = ":map <?> <!>"
-#vim.dotfilelocation = "~/.vimrc"
-#vim.defaultmodifier = "<leader><?>"
+vim.unmapcommand = ":unmap <?>"
+vim.mapcommand = ":map <?> <!>"
+vim.dotfilelocation = "~/.vimrc"
+vim.defaultmodifier = "<leader><?>"
 vim.opennewtab[:function] = ":tabnew<CR>"
+vim.selectprevioustab[:function] = ":tabprev<CR>" 
+vim.selectnexttab[:function] = ":tabnext<CR>" 
+vim.shifttableft[:function] = ":-tabmove<CR>" 
+vim.shifttabright[:function] = ":+tabmove<CR>" 
+vim.closecurrenttab[:function] = ":tabclose<CR>" 
+
 vim.write_shortcuts
 
 
