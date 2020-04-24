ae8de4510156920e707419683c1983c3d44cab1d
Eddie Drury <eddie.drury@gmail.com>
2020-04-24 16:52:11 +0800

    writing full keyboard changes to dotfile now

--- main.rb
+++ main.rb
@@ -13,18 +13,24 @@ end
 
 #Assign shortcuts to an individual app
 class App < Commands
-  attr_accessor :unmapcommand, :mapcommand, :dotfilelocation, :defaultmodifier
+  attr_accessor :unmapcommand, :mapcommand, :dotfilelocation, :defaultmodifier, :commentmarker
   def write_shortcuts
+    dotlines = "\n#{self.commentmarker*5} Universal Shortcuts start here\n"
     self.instance_variables.map do |attribute|
-      unless (attribute.to_s == "@unmapcommand") || (attribute.to_s == "@mapcommand") || (attribute.to_s == "@dotfilelocation") || (attribute.to_s == "@defaultmodifier")
+      unless (attribute.to_s == "@unmapcommand") || (attribute.to_s == "@mapcommand") || (attribute.to_s == "@dotfilelocation") || (attribute.to_s == "@defaultmodifier") || (attribute.to_s == "@commentmarker") 
         if function = self.instance_variable_get(attribute)[:function]
           command = self.instance_variable_get(attribute)[:defaultkey]
           shortcut = self.defaultmodifier.gsub("<?>",command) 
           line = self.mapcommand.gsub("<?>",shortcut)
-          puts line = line.gsub("<!>", function)
+          line = line.gsub("<!>", function)
+          dotlines += "#{line} \n"
         end
       end
     end
+    puts dotlines += "#{self.commentmarker*5} Universal Shortcuts end here\n\n"
+    f = File.open(File.expand_path(self.dotfilelocation), 'a')
+    f.write(dotlines)
+    f.close
   end
 end
 
--- vim.rb
+++ vim.rb
@@ -1,8 +1,14 @@
+#Initialise instance of the app
 vim = App.new
+
+#Define app specific variables
 vim.unmapcommand = ":unmap <?>"
 vim.mapcommand = ":map <?> <!>"
 vim.dotfilelocation = "~/.vimrc"
 vim.defaultmodifier = "<leader><?>"
+vim.commentmarker = "\""
+
+#Assign the functions to each command
 vim.opennewtab[:function] = ":tabnew<CR>"
 vim.selectprevioustab[:function] = ":tabprev<CR>" 
 vim.selectnexttab[:function] = ":tabnext<CR>" 
@@ -10,12 +16,5 @@ vim.shifttableft[:function] = ":-tabmove<CR>"
 vim.shifttabright[:function] = ":+tabmove<CR>" 
 vim.closecurrenttab[:function] = ":tabclose<CR>" 
 
+#Write the shortucts to the dotfile
 vim.write_shortcuts
-
-
-#  :OpenNewTab => {:defaultkey => "t"},
-#  :SelectPreviousTab => {:defaultkey => "j"},
-#  :SelectNextTab => {:defaultkey => "k"},
-#  :ShiftTabLeft => {:defaultkey => ","},
-#  :ShiftTabRight => {:defaultkey => "."},
-#  :CloseCurrentTab => {:defaultkey => "w"}
