0164ef7bfea6b644bc76f0e50ac8b358f84888c2
Eddie Drury <eddie.drury@gmail.com>
2020-05-30 12:33:42 +0800

    all shortcuts and functions in json

--- main.rb
+++ main.rb
@@ -1,54 +1,16 @@
 require 'ostruct'
+require 'json'
 
-#Default shortcuts
-$tabshash = {
-  "opennewtab" => {:defaultkey => "o"},
-  "selectprevioustab" => {:defaultkey => "j"},
-  "selectnexttab" => {:defaultkey => "k"},
-  "shifttableft" => {:defaultkey => ","},
-  "shifttabright" => {:defaultkey => "."},
-  "closecurrenttab" => {:defaultkey => "x"},
-  "selectfirsttab" => {:defaultkey => "0"},
-  "selectlasttab" => {:defaultkey => "$"},
-  "buffernext" => {:defaultkey => "l"},
-  "bufferprevious" => {:defaultkey => "h"},
-  "bufferclose" => {:defaultkey => "w"}
-}
-
-$viewshash = {
-  "stepforward" => {:defaultkey => "e"},
-  "stepback" => {:defaultkey => "y"},
-  "jumpforward" => {:defaultkey => "f"},
-  "jumpback" => {:defaultkey => "b"},
-  "find" => {:defaultkey => "/"},
-  "gotostart" => {:defaultkey => "gg"},
-  "gotoend" => {:defaultkey => "G"},
-  "zoomin" => {:defaultkey => "="},
-  "zoomout" => {:defaultkey => "-"},
-  "resetzoom" => {:defaultkey => "0"}
-}
-
-$fileshash = {
-  "openfile" => {:defaultkey => "o" },
-  "newfile" => {:defaultkey => "" },
-  "savefile" => {:defaultkey => "" },
-  "saveandclose" => {:defaultkey => "" },
-  "closenosave" => {:defaultkey => "" },
-  "savefileas" => {:defaultkey => "" },
-  "searchforfile" => {:defaultkey => "" },
-  "savesession" => {:defaultkey => "" },
-  "loadsession" => {:defaultkey => "" }
-}
-
+$defaultshortcuts = JSON.parse(File.read('default-shortcuts.json'))
 
 
 def write_shortcuts(app)
   dotlines = "\n#{app.commentmarker*5} Universal Shortcuts start here\n"
   app.marshal_dump.each do |attribute,value|
     unless (attribute.to_s == "unmapcommand") || (attribute.to_s == "mapcommand") || (attribute.to_s == "dotfilelocation") || (attribute.to_s == "defaultmodifier") || (attribute.to_s == "commentmarker")
-      if (command = value[:defaultkey]) &&
+      if (command = value["defaultkey"]) &&
           (command.length > 0) &&
-          (function = value[:function]) &&
+          (function = value["function"]) &&
           (function.length > 0)
         shortcut = app.defaultmodifier.gsub("<?>",command)
         line = app.mapcommand.gsub("<?>",shortcut)
--- vim.rb
+++ vim.rb
@@ -1,65 +1,15 @@
 #Initialise instance of the app
 vim = OpenStruct.new
 
-#Define app specific variables
-vim.unmapcommand = ":unmap <?>"
-vim.mapcommand = ":map <?> <!>"
-vim.dotfilelocation = "~/.vimrc"
-vim.defaultmodifier = "<leader><?>"
-vim.commentmarker = "\""
-
-#Assign the functions to each command
-
-#Tabs
-vimtabshash = {
-"opennewtab" => {:function => ":tabnew<CR>"},
-"selectprevioustab" => {:function => ":tabprev<CR>"},
-"selectnexttab" => {:function => ":tabnext<CR>"},
-"shifttableft" => {:function => ":-tabmove<CR>"},
-"shifttabright" => {:function => ":+tabmove<CR>"},
-"closecurrenttab" => {:function => ":tabclose<CR>"},
-"selectfirsttab" => {:function => ":tabfirst<CR>"},
-"selectlasttab" => {:function => ":tablast<CR>"},
-"buffernext" => {:function => ":bnext<CR>"},
-"bufferprevious" => {:function => ":bprevious<CR>"},
-"bufferclose" => {:function => ":bd<CR>"}
-}
-
-#View
-vimviewshash = {
-"stepforward" => {:function => "<C-E>" },
-"stepback" => {:function => "<C-Y>" },
-"jumpforward" => {:function => "<C-F>" },
-"jumpback" => {:function => "<C-B>" },
-"find" => {:function => "/" },
-"gotostart" => {:function => "gg" },
-"gotoend" => {:function => "G" },
-"zoomin" => {:function => "" },
-"zoomout" => {:function => "" },
-"resetzoom" => {:function => "" },
-}
-
-#Files
-vimfileshash = {
-"openfile" => {:function => ":o " },
-"newfile" => {:function => ":o " },
-"savefile" => {:function => ":w<CR> " },
-"saveandclose" => {:function => ":wq<CR>" },
-"closenosave" => {:function => ":q!<CR>" },
-"savefileas" => {:function => ":w " },
-"searchforfile" => {:function => "" },
-"savesession" => {:function => ":mks " },
-"loadsession" => {:function => ":source " },
-}
+vimsettings = JSON.parse(File.read('vim.json'))
 
 #Write the shortucts to the dotfile
-injectfunctions($tabshash,vimtabshash).each { |k,v| vim.public_send("#{k}=", v) }
-puts injectfunctions($fileshash,vimfileshash)
-injectfunctions($fileshash,vimfileshash).each { |k,v| vim.public_send("#{k}=", v) }
-injectfunctions($viewshash,vimviewshash).each { |k,v| vim.public_send("#{k}=", v) }
+puts vimsettings["vimvariableshash"]
+vimsettings["vimvariableshash"].each { |k,v| vim.public_send("#{k}=", v) }
+injectfunctions($defaultshortcuts["tabshash"],vimsettings["vimtabshash"]).each { |k,v| vim.public_send("#{k}=", v) }
+injectfunctions($defaultshortcuts["fileshash"],vimsettings["vimfileshash"]).each { |k,v| vim.public_send("#{k}=", v) }
+injectfunctions($defaultshortcuts["viewshash"],vimsettings["vimviewshash"]).each { |k,v| vim.public_send("#{k}=", v) }
 write_shortcuts(vim)
 
-
-
-
-
+#masterhash = {"vimvariableshash" => vimvariableshash, "vimfileshash" => vimfileshash, "vimviewshash" => vimviewshash, "vimtabshash" => vimtabshash}
+#puts JSON.pretty_generate(masterhash)
