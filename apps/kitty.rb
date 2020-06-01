12b9b9411e576f9c6c8a2831f1d0856b50e7d588
Eddie Drury <eddie.drury@gmail.com>
2020-06-01 15:24:39 +0800

    all move to json

--- apps/kitty.rb
+++ apps/kitty.rb
@@ -1,64 +1,6 @@
 #Initialise instance of the app
-require 'json'
-#kitty = App.new
-
-#Define app specific variables
-output = {
-"appvariables" => {
-"unmapcommand" => "",
-"mapcommand" => "map <?> <!>",
-"dotfilelocation" => "~/.config/kitty/kitty.conf",
-"defaultmodifier" => "ctrl+shift+<?>",
-"commentmarker" => "#",
-},
-
-#Assign the functions to each command
-
-#Tabs
-"tabshash" => {
-"opennewtab" => {"function" => "new_tab_with_cwd"},
-"selectprevioustab" => {"function" => "previous_tab" },
-"selectnexttab" => {"function" => "next_tab" },
-"shifttableft" => {"function" => "move_tab_backward" },
-"shifttabright" => {"function" => "move_tab_forward" },
-"closecurrenttab" => {"function" => "close_tab" },
-"selectfirsttab" => {"function" => "goto_tab 1" },
-"selectlasttab" => {"function" => "goto_tab 99" },
-"buffernext" => {"function" => ""},
-"bufferprevious" => {"function" => ""},
-"bufferclose" => {"function" => ""}
-},
-
-#View
-"viewshash" => {
-"stepforward" => {"function" => "scroll_line_down" },
-"stepback" => {"function" => "scroll_line_up" },
-"jumpforward" => {"function" => "scroll_page_down" },
-"jumpback" => {"function" => "scroll_page_up" },
-"find" => {"function" => "show_scrollback" },
-"gotostart" => {"function" => "scroll_home" },
-"gotoend" => {"function" => "scroll_end" },
-"zoomin" => {"function" => "change_font_size all +1.0" },
-"zoomout" => {"function" => "change_font_size all -1.0" },
-"resetzoom" => {"function" => "change_font_size all 0" },
-},
-
-#Files
-"fileshash" => {
-"openfile" => {"function" => "" },
-"newfile" => {"function" => "" },
-"savefile" => {"function" => "" },
-"saveandclose" => {"function" => "" },
-"closenosave" => {"function" => "" },
-"savefileas" => {"function" => "" },
-"searchforfile" => {"function" => "" },
-"savesession" => {"function" => "" },
-"loadsession" => {"function" => "" },
-}
-}
-
+kitty = App.new
 
 #Write the shortucts to the dotfile
 #kitty.write_shortcuts
 
-puts JSON.pretty_generate(output)
--- apps/qutebrowser.rb
+++ apps/qutebrowser.rb
@@ -1,61 +1,7 @@
 #Initialise instance of the app
-require 'json'
-#qutebrowser = App.new
+qutebrowser = App.new
 
 #Define app specific variables
-output = {
-"appvariables" => {
-"unmapcommand" => "",
-"mapcommand" => "config.bind('<?>', '<!>')",
-"dotfilelocation" => "~/.config/qutebrowser/config.py",
-"defaultmodifier" => "<?>",
-"commentmarker" => "#",
-},
-
-#Assign the functions to each command
-
-#Tabs
-"tabshash" => {
-"opennewtab" => {"function" => "open -t"},
-"selectprevioustab" => {"function" => "tab-prev" },
-"selectnexttab" => {"function" => "tab-next" },
-"shifttableft" => {"function" => "tab-move -" },
-"shifttabright" => {"function" => "tab-move +" },
-"closecurrenttab" => {"function" => "tab-close" },
-"selectfirsttab" => {"function" => "tab-focus 1" },
-"selectlasttab" => {"function" => "tab-focus -1" },
-"buffernext" => {"function" => "forward"},
-"bufferprevious" => {"function" => "back"},
-"bufferclose" => {"function" => ""},
-},
-
-#View
-"viewshash" => {
-"stepforward" => {"function" => "scroll down" },
-"stepback" => {"function" => "scroll up" },
-"jumpforward" => {"function" => "scroll-page 0 1" },
-"jumpback" => {"function" => "scroll-page 0 -1" },
-"find" => {"function" => "set-cmd-text  =>search " },
-"gotostart" => {"function" => "scroll-to-perc 0" },
-"gotoend" => {"function" => "scroll-to-perc 100" },
-"zoomin" => {"function" => "zoom-in" },
-"zoomout" => {"function" => "zoom-out" },
-"resetzoom" => {"function" => "zoom" },
-},
-
-#Files
-"fileshash" => {
-"openfile" => {"function" => "set-cmd-text  =>open " },
-"newfile" => {"function" => "set-cmd-text  =>open -t " },
-"savefile" => {"function" => "" },
-"saveandclose" => {"function" => "" },
-"closenosave" => {"function" => "" },
-"savefileas" => {"function" => "download" },
-"searchforfile" => {"function" => "" },
-"savesession" => {"function" => "set-cmd-text  =>save-session -o " },
-"loadsession" => {"function" => "set-cmd-text  =>load-session " },
-}
-}
 
 #Qutebrowser requires direct user configuations via file config.py. Create file if it doesn't exist and load existing user settings. 
 #if File.file?("#{ENV['HOME']}/.config/qutebrowser/config.py") == false
@@ -64,5 +10,4 @@ output = {
 #  configfile.close
 #end
 
-puts JSON.pretty_generate(output)
 #Write the shortucts to the dotfile
