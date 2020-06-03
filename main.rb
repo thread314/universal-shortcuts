8c390aea95ba6acb1ea5080fb03775e998255aae
Eddie Drury <eddie.drury@gmail.com>
2020-06-03 12:30:12 +0800

    reorganised files

--- main.rb
+++ main.rb
@@ -3,9 +3,32 @@ require 'json'
 
 $defaultshortcuts = JSON.parse(File.read('default-shortcuts.json'))
 
-#Write all dotfile commands to output/file
-def write_shortcuts(app)
-  dotlines = "\n#{app.commentmarker*5} Universal Shortcuts for #{app} start here\n"
+#Combine default shortcuts and app functions into one hash
+def injectfunctions(shortcuthash,functionhash)
+  combinedhash = {}
+  functionhash.each do |k,v|
+    combinedhash[k] = functionhash[k].merge(shortcuthash[k])
+  end
+  return combinedhash
+end
+
+#Create object that contains all shortcuts and functions
+def createappobject(appname)
+  app = OpenStruct.new
+  app.name = appname
+  appsettings = JSON.parse(File.read("apps/#{appname}.json"))
+  appsettings["appvariables"].each { |k,v| app.public_send("#{k}=", v) }
+  injectfunctions($defaultshortcuts["tabshash"],appsettings["tabshash"]).each { |k,v| app.public_send("#{k}=", v) }
+  injectfunctions($defaultshortcuts["fileshash"],appsettings["fileshash"]).each { |k,v| app.public_send("#{k}=", v) }
+  injectfunctions($defaultshortcuts["viewshash"],appsettings["viewshash"]).each { |k,v| app.public_send("#{k}=", v) }
+  construct_dotfile_entry(app)
+end
+
+#Put together all necessary dotfile commands for this app
+def construct_dotfile_entry(app)
+  dotlines = "\n#{app.commentmarker*5} Universal Shortcuts start here - do not edit this line/block\n"
+  dotlines += "#{app.commentmarker*5} Be aware any changes you manually add in this block will be over-written next time you run universal-shortcuts. \n"
+  dotlines += "#{app.commentmarker*5} If you want to edit the below, you should do so through universal-shortcuts\n"
   app.marshal_dump.each do |attribute,value|
     unless (attribute.to_s == "unmapcommand") || (attribute.to_s == "mapcommand") || (attribute.to_s == "dotfilelocation") || (attribute.to_s == "defaultmodifier") || (attribute.to_s == "commentmarker")
       if (command = value["defaultkey"]) &&
@@ -19,41 +42,33 @@ def write_shortcuts(app)
       end
     end
   end
-  puts dotlines += "#{app.commentmarker*5} Universal Shortcuts end here\n\n"
-  #Qutebrowser requires direct user configuations via file config.py. Create file if it doesn't exist and load existing user settings.
-  #if File.file?("#{ENV['HOME']}/.config/qutebrowser/config.py") == false
-  #  configfile = File.new("#{ENV['HOME']}/.config/qutebrowser/config.py")
-  #  configfile.puts("config.load_autoconfig()\n\n")
-  #  configfile.close
-  #end
-  #    f = File.open(File.expand_path(app.dotfilelocation), 'a')
-  #    f.write(dotlines)
-  #    f.close
+  dotlines += "#{app.commentmarker*5} Universal Shortcuts end here - do not edit this line/block\n\n"
+  return dotlines
 end
 
-#Combine default shortcuts and app functions into one hash
-def injectfunctions(shortcuthash,functionhash)
-  combinedhash = {}
-  functionhash.each do |k,v|
-    combinedhash[k] = functionhash[k].merge(shortcuthash[k])
+#Print dotfile entry or write directly to dotfile
+def output_dotfile_entry
+  puts "Would you like to (p)rint the output or (w)rite directly to the dotfile/s?"
+  answer = gets.chomp
+  case(answer)
+  when "p"
+    puts dotlines
+  when "w"
+    #Qutebrowser requires direct user configuations via file config.py. Create file if it doesn't exist and load existing user settings.
+    if File.file?("#{ENV['HOME']}/.config/qutebrowser/config.py") == false
+      configfile = File.new("#{ENV['HOME']}/.config/qutebrowser/config.py")
+      configfile.puts("config.load_autoconfig()\n\n")
+      configfile.close
+    end
+    f = File.open(File.expand_path(app.dotfilelocation), 'a')
+    f.write(dotlines)
+    f.close
   end
-  return combinedhash
-end
-
-#Create object that contains all shortcuts and functions
-def createappobject(appname)
-  app = OpenStruct.new
-  appsettings = JSON.parse(File.read("apps/#{appname}.json"))
-  appsettings["appvariables"].each { |k,v| app.public_send("#{k}=", v) }
-  injectfunctions($defaultshortcuts["tabshash"],appsettings["tabshash"]).each { |k,v| app.public_send("#{k}=", v) }
-  injectfunctions($defaultshortcuts["fileshash"],appsettings["fileshash"]).each { |k,v| app.public_send("#{k}=", v) }
-  injectfunctions($defaultshortcuts["viewshash"],appsettings["viewshash"]).each { |k,v| app.public_send("#{k}=", v) }
-  write_shortcuts(app)
 end
 
 
-createappobject("vim")
-createappobject("kitty")
-createappobject("emacs")
-createappobject("ranger")
-createappobject("qutebrowser")
+#createappobject("vim")
+#createappobject("kitty")
+#createappobject("emacs")
+#createappobject("ranger")
+#createappobject("qutebrowser")
